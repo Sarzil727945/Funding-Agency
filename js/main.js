0045c36a843f294a04a989b8d4e3ad90bbfa8996
Sarzil727945 <sarzilmuntaha.gmail.com>
2022-12-02 21:36:01 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+ // navber js part start
+
+    $(document).ready(function(){
+        $(".nav1 a").click(function(){
+            $(this).css("color","red");
+        })
+    
+        $(".nav1 a").blur(function(){
+            $(this).css("color","black");
+        })
+            
+    })
+                                            
+ // navber js part ends
+
+//  section js part start 
+
+            $(document).ready(function(){
+                    $(".twitter ").click(function(){
+                        alert("This is 'twitter'.");
+                    });
+                });
+                $(document).ready(function(){
+                    $(".facebook ").click(function(){
+                        alert("This is 'facebook'.");
+                    });
+                });
+                $(document).ready(function(){
+                    $(".youtube ").click(function(){
+                        alert("This is 'youtube'.");
+                    });
+                });
+
+ //  section js part ends
+
